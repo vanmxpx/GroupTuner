4161f9a9803d48104350600114cc9a3263fc1690
Nikita Tsyhankov <nikita1996.tsyhankov@gmail.com>
2016-12-20 00:03:47 +0200

    BaseApp

---
+++ SoundCapture.BL/DeviceManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+
+namespace SoundCapture.BL
+{
+    public class DeviceManager
+    {
+        MMDeviceEnumerator deviceEnum;
+        MMDeviceCollection deviceCol;
+
+        private void UpdateDevices()
+        {
+            MMDeviceEnumerator deviceEnum = new MMDeviceEnumerator();
+            MMDeviceCollection deviceCol = deviceEnum.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active);
+        }
+
+        public MMDevice[] GetActiveDevices()
+        {
+            UpdateDevices();
+            return deviceCol.ToArray();
+        }
+
+        public MMDevice GetDeviceByNumber(Int32 deviceAt)
+        {
+            UpdateDevices();
+            return deviceCol.ElementAt(deviceAt);
+        }
+
+        public MMDevice GetDefaultDevice()
+        {
+            UpdateDevices();
+            return deviceEnum.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Multimedia);
+        }
+    }
+}
---
+++ SoundCapture.BL/SoundCapture.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+
+namespace SoundCapture.BL
+{
+    public class SoundCapture
+    {
+        private IWaveIn _waveIn;
+        private WaveBuffer _buffer;
+        private MMDevice _device;
+
+        public SoundCapture()
+        {
+        }
+
+        public void Start()
+        {
+            _waveIn = new WasapiCapture(_device);
+            _waveIn.DataAvailable += new EventHandler<WaveInEventArgs>(_waveIn_DataAvailable);
+            _waveIn.WaveFormat = new WaveFormat(44100, 16, 1);
+
+            _waveIn.StartRecording();
+        }
+
+        public void Stop()
+        {
+            if (_waveIn != null)
+                _waveIn.StopRecording();
+            _waveIn.Dispose();
+            _waveIn = null;
+        }
+
+        private void _waveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            _buffer.BindTo(e.Buffer);
+            ProcessData(_buffer.FloatBuffer);
+        }
+
+        protected abstract void ProcessData(float[] data);
+    }
+}
